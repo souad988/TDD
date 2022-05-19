9630652ad174c66e23b86200c6d09c29575ef1a2
Bavon101 <bavonfate@gmail.com>
2022-05-19 13:37:11 +0300

    Hotfix: test with raise exception to use expect error with message

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,72 +1,68 @@
+require_relative 'spec_helper'
 
 describe 'class solver' do
-     before :each do
-        @solver = Solver.new
-     end
-    context 'tests for factorial' do
-        it 'return instance of class solver' do
-        expect(@solver).to be_instance_of Solver 
-        end
-        
-        it 'factorial accepts one argument' do
-        expect(@solver.factorial(0)).to eql 1 
-        end
-        
-        it 'raises an exeption for negative arguments' do
-            expect(@solver.factorial(-2)).to raise_exception
-        end
-
-        it 'returns 6 as factorial of 3' do
-            expect(@solver.factorial(3)).to eql 6
-        end
-
-        it 'raises an exception for string value' do
-            expect(@solver.factorial("ab")).to raise_exception
-        end 
+  before :each do
+    @solver = Solver.new
+  end
+  context 'tests for factorial' do
+    it 'return instance of class solver' do
+      expect(@solver).to be_instance_of Solver
     end
 
-    context 'tests for reverse methode' do
-        it 'raise exception for non string values' do
-           expect(@solver.reverse(333)).to raise_exception
-        end
-
-        it 'reverses hello to be olleh' do
-            expect(@solver.reverse('hello')).to eql 'olleh'
-        end
-
-        it 'reverses microve\'rse and return esr\'evorcim ' do
-            expect(@solver.reverse('microve\'rse')).to eql 'esr\'evorcim'
-        end
+    it 'factorial return 1 for 0' do
+      expect(@solver.factorial(0)).to eql 1
     end
 
-    context 'tests for fizzbuzz methode' do
-
-        it 'raise an exception for non integer values' do
-           expect(@solver.fizzbuzz('55')).to raise_exception
-        end
-
-        it 'returns fizz for 6' do
-            expect(@solver.fizzbuzz(6)).to eql 'fizz'
-        end
-
-        it 'returns fizzbuzz for 15' do
-            expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
-        end
-
-        it 'returns buzz for 10' do
-            expect(@solver.fizzbuzz(10)).to eql 'buzz'
-        end
-
-        it 'returns "7" for 7 ' do
-            expect(@solver.fizzbuzz(7)).to eql "7"
-        end
-
-        it 'returns fizz for -18' do
-            expect(@solver.fizzbuzz(-18)).to eql 'fizz'
-        end
-
-
+    it 'raises an exeption for negative arguments' do
+      expect { @solver.factorial(-2) }.to raise_error('num has to be >= 0')
     end
 
+    it 'returns 6 as factorial of 3' do
+      expect(@solver.factorial(3)).to eql 6
+    end
 
+    it 'raises an exception for string value' do
+      expect { @solver.factorial('ab') }.to raise_error('Only numbers are accepted')
+    end
+  end
+
+  context 'tests for reverse method' do
+    it 'raise exception for non string values' do
+      expect { @solver.reverse(333) }.to raise_error('Only String values are accepted')
+    end
+
+    it 'reverses hello to be olleh' do
+      expect(@solver.reverse('hello')).to eql 'olleh'
+    end
+
+    it 'reverses microve\'rse and return esr\'evorcim ' do
+      expect(@solver.reverse('microve\'rse')).to eql 'esr\'evorcim'
+    end
+  end
+
+  context 'tests for fizzbuzz methode' do
+    it 'raise an exception for non integer values' do
+      expect { @solver.fizzbuzz('55') }.to raise_error('Only numbers are accepted')
+    end
+
+    it 'returns fizz for 6' do
+      expect(@solver.fizzbuzz(6)).to eql 'fizz'
+    end
+
+    it 'returns fizzbuzz for 15' do
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+
+    it 'returns buzz for 10' do
+      expect(@solver.fizzbuzz(10)).to eql 'buzz'
+    end
+
+    it 'returns "7" for 7 ' do
+      expect(@solver.fizzbuzz(7)).to eql '7'
+    end
+
+    it 'returns fizz for -18' do
+      expect(@solver.fizzbuzz(-18)).to eql 'fizz'
+    end
+  end
 end
